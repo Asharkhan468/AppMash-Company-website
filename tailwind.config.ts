6d8af613e3f91cbe19a17ee1a66a35fa8608a805
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-08-11 17:36:13 +0500

    SOME ISSUES

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}"
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   safelist: [
-    'clip-bottom-right',
-    'clip-top-left',
-    'PhoneInputInput',
-    'PhoneInputCountry',
-    'PhoneInput',
-    'text-red-500', // agar dynamic text colors use ho rahe ho
-    'bg-primary1',
-    'bg-primary2',
-    'bg-neutral1',
-    'bg-neutral2',
-    'bg-neutral3',
-    'bg-neutral4',
-    'bg-secondary1',
-    'bg-secondary2',
-    { pattern: /w-\[.*\]/ },
-  { pattern: /h-\[.*\]/ },
-  { pattern: /right-\[.*\]/ },
-  { pattern: /lg:right-\[.*\]/ },
-  { pattern: /max-w-\[.*\]/ },
-  { pattern: /md:order-.*/ },
+    'w-[160px]',
+    'w-[200px]',
+    'w-[260px]',
+    'w-[340px]',
+    'w-[440px]',
+    'h-[160px]',
+    'h-[200px]',
+    'h-[260px]',
+    'h-[340px]',
+    'h-[440px]',
+    'max-w-[90%]',
+    // agar tum xs: prefix use karte ho to:
+    'xs:text-3xl',
+    'xs:w-[200px]'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
