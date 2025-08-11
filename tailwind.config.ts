f5f8fdb5c68d643aec608279728a261ac89e691a
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-08-11 14:43:52 +0500

    Some Issue fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,12 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
- theme: {
-  screens: {
-    xs: "480px",
-    sm: "640px",
-    md: "768px",
-    lg: "1024px",
-    xl: "1280px",
+  theme: {
+    extend: {},
   },
-},
-  safelist: [
-    "bg-primary2",
-    "from-primary2/40",
-    "text-primary1",
-    "hover:bg-primary2/90"
-  ],
   plugins: [],
 };
