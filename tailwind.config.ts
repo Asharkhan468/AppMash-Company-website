a6363552c9c64ab275965df31b6d60923de18fc2
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-08-11 17:02:31 +0500

    ISSUE FIX

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,13 +4,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   safelist: [
     'clip-bottom-right',
     'clip-top-left',
     'PhoneInputInput',
     'PhoneInputCountry',
-    'PhoneInput'
+    'PhoneInput',
+    'text-red-500', // agar dynamic text colors use ho rahe ho
+    'bg-primary1',
+    'bg-primary2',
+    'bg-neutral1',
+    'bg-neutral2',
+    'bg-neutral3',
+    'bg-neutral4',
+    'bg-secondary1',
+    'bg-secondary2'
   ],
   theme: {
     extend: {},
