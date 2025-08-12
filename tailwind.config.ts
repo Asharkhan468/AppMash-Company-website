17c3294ed095686599bce22fd0cc8a77e495c45e
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-08-12 18:13:17 +0500

    TRY ISSUE FIX

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary1: "#d0363c",
+        primary2: "#b32724",
+        secondary1: "#ffffff",
+      },
+    },
+  },
+  safelist: [
+    "bg-primary2",
+    "bg-secondary1",
+    "text-primary1",
+    "text-primary2",
+    "bg-white/50"
+  ],
   plugins: [],
 };
