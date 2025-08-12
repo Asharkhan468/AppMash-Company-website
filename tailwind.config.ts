528633f36950d336d86942639ef629972cc7ba45
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-08-12 18:09:25 +0500

    SOME ISSUE FIX

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,22 +6,6 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  safelist: [
-    "bg-primary2",
-    "bg-secondary1",
-    "text-primary1",
-    "text-primary2",
-    "text-secondary1",
-    "from-primary2",
-    "via-black",
-    "to-primary2",
-    "md:grid-cols-2",
-    "flex",
-    "items-center",
-    "justify-center",
-  ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 };
