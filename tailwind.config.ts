c6a639abc000fe5f6d6ca43384d262c14f89c1f0
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-08-13 13:37:47 +0500

    SOME CHANGES FOR FIX DEPLOYMENT ISSUE

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,8 @@ interface MyConfig extends Config {
 
 const config: MyConfig = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   safelist: [
     "min-h-[600px]",
