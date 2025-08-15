07481a3e0d86f47bf5f1c3b45592d686191a616b
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-08-15 12:25:37 +0500

    SOME MODIFICATION

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
   safelist: [
-    "bg-primary1",
-    "bg-primary2",
-    "bg-secondary1",
-    "text-primary1",
-    "text-primary2",
-    "text-secondary1",
     "min-h-[300px]",
     "md:min-h-[550px]",
-    "max-w-[440px]",
+    "max-w-[440px]"
   ],
   theme: {
-    extend: {
-      colors: {
-        primary1: "#d0363c",
-        primary2: "#b32724",
-        secondary1: "#ffffff",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
